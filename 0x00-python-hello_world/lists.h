502262547ff5287bf34fb280462e6de2b5a8ec9d
Samuel Armah <samuelarmah817@gmail.com>
2023-06-05 16:04:51 -0700

    Updated

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -5,20 +5,21 @@
 
 /**
  * struct listint_s - linked list
- * @i: integer
+ * @n: integer
  * @next: points to the next node
  * 
  * Description: linked list node structure
  */
 
 typedef struct listint_s
+
 {
-	int i;
+	int n;
 	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int i);
+listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
