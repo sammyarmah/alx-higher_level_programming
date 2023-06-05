1dd934cc42d91ae28ecc50a05dec890fa09a8157
Samuel Armah <samuelarmah817@gmail.com>
2023-06-05 15:51:58 -0700

    lists.h

---
+++ 0x00-python-hello_world/lists.h
@@ -0,0 +1,25 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/**
+ * struct listint_s - linked list
+ * @i: integer
+ * @next: points to the next node
+ * 
+ * Description: linked list node structure
+ */
+
+typedef struct listint_s
+{
+	int i;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int i);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
+
+#endif /* LISTS_H */
