33d072b8cca8b4267e65a67190d31b48c372e1c1
Samuel Armah <samuelarmah817@gmail.com>
2023-06-08 22:23:01 -0700

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,34 +1,26 @@
 #!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
 
-if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
-    import sys
+if len(sys.argv) != 4:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    sys.exit(1)
 
-    if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+a = int(sys.argv[1])
+operator = sys.argv[2]
+b = int(sys.argv[3])
 
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in ops:
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+reult = None
+if operator == '+':
+    result = add(a, b)
+elif operator == '-':
+    result = sub(a, b)
+elif operator == '*':
+    result = mul(a, b)
+elif operator == '/':
+    result = div(a, b)
+else:
+    print(Unknown operator. Available operators: +, -, * and /")
+    sys.exit(1)
 
-    a = int(sys.argv[1])
-    operator = sys.argv[2]
-    b = int(sys.argv[3])
-
-    result = None
-    if operator == "+":
-        result = ops[operator](a, b)
-    elif operator == "-":
-        result = ops[operator](a, b)
-    elif operator == "*":
-        result = ops[operator](a, b)
-    elif operator == "/":
-        if b == 0:
-            print("Error: Division by zero is not allowed.")
-            sys.exit(1)
-        result = ops[operator](a, b)
-
-    print("{} {} {} = {}".format(a, operator, b, result))
-
+print(f"{a} {operator} {b} = {result}")
